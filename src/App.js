0dba0320eba43a07521af9960f07573ba210f320
Deeksha Shekar <deeksha.somashekar@gmail.com>
2022-07-30 22:48:03 +0530

    better font

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { usePosition } from 'use-position';
+
+import React from 'react'
+import Weather from './Weather';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const App = () => {
  
- const [weather, setWeather] = useState("");
- const [temp, setTemp] = useState("");
- const { latitude, longitude } = usePosition();
- useEffect(() => {
-   if (latitude && longitude) {
-     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-       
-     ).then((response) => {
-       setWeather(response.data.weather[0].description);
-       setTemp(Math.round(response.data.main.temp));
-     });
-   }
- }, [latitude, longitude]);
   return (
-    <>
-      <h1>{temp}</h1>
-      <h1>{weather}</h1>
-    </>
+    <Weather/>
   );
 }
 
